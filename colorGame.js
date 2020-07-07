faa56d68f5a2af055ab877563b4db236d8a97dba
Edward Leung <edward_leung_12@hotmail.com>
2020-07-07 20:18:33 +0100

    update

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,10 @@
 var colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)',
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
 ]
 
 var squares = document.querySelectorAll('.square');
@@ -20,7 +20,12 @@ for (var i = 0; i < squares.length; i++) {
     //add click listeners to squares
     squares[i].addEventListener('click', function() {
         //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
-
+        if(clickedColor === pickedColor) {
+            alert('Correct!');
+        } else {
+            this.style.backgroundColor = '#232323';
+        }
     });
 }
